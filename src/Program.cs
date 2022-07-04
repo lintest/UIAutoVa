0e323874ac62506c65d4721f2be7167bea35e249
Kandrashin Denis <mail@lintest.ru>
2022-07-04 23:41:07 +0300

    Версия 0.0.1

--- src/Program.cs
+++ src/Program.cs
@@ -33,79 +33,66 @@ namespace FocusChanged {
                 }
                 Console.WriteLine("Monitoring... Hit enter to end.");
                 Console.ReadLine();
+                writer = null;
             }
         }
 
-        private static void WriteEvent(object src, String eventId) {
+        private static String GetEventInfo(object src, AutomationEventArgs args, String eventId) {
             AutomationElement element = src as AutomationElement;
-            if (element != null) {
-                AutomationElement.AutomationElementInformation current;
-                try { 
-                    current = element.Current; 
-                } catch {
-                    return;
+            if (element == null) {
+                return null;
+            }
+            try {
+                int processId = 0;
+                var rect = element.Current.BoundingRectangle;
+                try { processId = element.Current.ProcessId; } catch { }
+                using (Process process = Process.GetProcessById(processId)) {
+                    var summary = new {
+                        EventId = eventId,
+                        Name = element.Current.Name,
+                        ProcessId = element.Current.ProcessId,
+                        AutomationId = element.Current.AutomationId,
+                        Rectangle = new {
+                            Left = rect.Left != Double.NaN ? rect.Left : 0,
+                            Top = rect.Top != Double.NaN ? rect.Top : 0,
+                            Width = rect.Width != Double.NaN ? rect.Width : 0,
+                            Height = rect.Height != Double.NaN ? rect.Height : 0,
+                        },
+                    };
+                    var options = new JsonSerializerOptions { WriteIndented = false };
+                    return JsonSerializer.Serialize(summary, options);
                 }
-                try {
-                    int processId = 0;
-                    var rect = current.BoundingRectangle; 
-                    try { processId = current.ProcessId; } catch { }
-                    using (Process process = Process.GetProcessById(processId)) {
-                        var summary = new {
-                            EventId = eventId,
-                            Name = current.Name,
-                            ProcessId = current.ProcessId,
-                            AutomationId = current.AutomationId,
-                            Rectangle = new {
-                                Left = rect.Left != Double.NaN ? rect.Left : 0,
-                                Top = rect.Top != Double.NaN ? rect.Top : 0,
-                                Width = rect.Width != Double.NaN ? rect.Width : 0,
-                                Height = rect.Height != Double.NaN ? rect.Height : 0,
-                            },
-                        };
-                        var options = new JsonSerializerOptions { WriteIndented = false };
-                        string jsonString = JsonSerializer.Serialize(summary, options);
-                        writer.WriteLine(jsonString + ",");
-                    }
-                }
-                catch (Exception e) {
-                    writer.WriteLine("{{Error: \"{0}\"}}", e.Message);
-                }
+            }
+            catch {
+                return null;
+            }
+        }
+
+        private static void WriteEvent(object src, AutomationEventArgs args, String eventId) {
+            var jsonString = GetEventInfo(src, args, "eventId");
+            if (!String.IsNullOrEmpty(jsonString)) {
+                writer.WriteLine(GetEventInfo(src, args, eventId) + ",");
             }
         }
 
         private static void OnFocusChangedHandler(object src, AutomationFocusChangedEventArgs args) {
-            WriteEvent(src, "FocusChanged");
+            WriteEvent(src, args, "FocusChanged");
         }
 
         private static void OnWindowOpened(object src, AutomationEventArgs args) {
             AutomationElement element = src as AutomationElement;
             if (element != null) {
-                WriteEvent(src, "WindowOpened");
-                try {
-                    int processId = element.Current.ProcessId;
-                    using (Process process = Process.GetProcessById(processId)) {
-                        var summary = new {
-                            EventId = "WindowClosed",
-                            Name = element.Current.Name,
-                            ProcessId = element.Current.ProcessId,
-                            AutomationId = element.Current.AutomationId,
-                        };
-                        AutomationEventHandler closeFunction = (object sender, AutomationEventArgs e) => {
-                            var options = new JsonSerializerOptions { WriteIndented = false };
-                            string jsonString = JsonSerializer.Serialize(summary, options);
-                            writer.WriteLine(jsonString + ",");
-                        };
-                        Automation.AddAutomationEventHandler(WindowPattern.WindowClosedEvent, element, TreeScope.Element, closeFunction);
-                    }
-                }
-                catch (Exception e) {
-                    writer.WriteLine("{{Error: \"{0}\"}}", e.Message);
-                }
+                WriteEvent(src, args, "WindowOpened");
+                var jsonString = GetEventInfo(src, args, "WindowClosed");
+                AutomationEventHandler closeFunction = (object sender, AutomationEventArgs e) => {
+                    writer.WriteLine(jsonString + ",");
+                };
+                Automation.AddAutomationEventHandler(WindowPattern.WindowClosedEvent, element, TreeScope.Element, closeFunction);
             }
         }
 
         private static void OnAutomationEventHandler(object sender, AutomationEventArgs args) {
-            WriteEvent(sender, events[args.EventId]);
+            WriteEvent(sender, args, events[args.EventId]);
         }
     }
 }
